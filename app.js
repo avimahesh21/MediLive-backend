be8298604359a017b7fba60c497949dd4057e9d0
avi22vt <avi22@vt.edu>
2024-02-16 23:53:37 -0800

    backend commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+var cors = require('cors')
+
+
+const app = express();
+const PORT = 3001;
+
+
+app.use(cors());
+
+
+app.get('/weather', (req, res)=>{
+    const temperatureData = {
+        temperature: 74
+      };
+   
+      // Send the JSON object as the response
+      res.json(temperatureData);
+});
+
+
+app.listen(PORT, (error) =>{
+  if(!error)
+    console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+  else
+    console.log("Error occurred, server can't start", error);
+  }
+);
